63edc817dac3cf7367772fa16d169af15dadca89
shylock <tcath2s@gmail.com>
2018-03-25 14:46:53 +0800

    add bool & QoS value check

--- inc/mqtt_packet.h
+++ inc/mqtt_packet.h
@@ -13,12 +13,24 @@
 #include "mqtt_var_header.h"
 #include "mqtt_payload.h"
 
+#include <assert.h>
+
 /*  \brief evaluate the value of bits in uint variable
  *  \param uint the packed uint value
  *  \param msk the and operator mask
  *  \param offset the bits offset to bit 0
  * */
 #define MQTT_PACKET_SEGMENT_EVAL(uint,msk,offset) ((uint&msk) >> offset)
+
+/*  \brief check packet flag value of boolean type
+ *  \param byte value of boolean
+ * */
+#define MQTT_PACKET_FLAG_VAL_BOOL_CHECK(byte) assert(0 == byte && 1 == byte)
+
+/*  \brief check packet flag QoS value 
+ *  \param byte value of QoS
+ * */
+#define MQTT_PACKET_FLAG_VAL_QoS_CHECK(byte)  assert(0 <= byte && 2 >= byte)
 
 #ifdef __cplusplus
 	}
