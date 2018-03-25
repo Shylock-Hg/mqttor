31172c716c2b718504b6d114cd37de0caad708cb
shylock <tcath2s@gmail.com>
2018-03-25 20:24:38 +0800

    define packet pack interface

--- inc/mqtt_packet.h
+++ inc/mqtt_packet.h
@@ -32,6 +32,196 @@
  * */
 #define MQTT_PACKET_FLAG_VAL_QoS_CHECK(byte)  assert(0 <= byte && 2 >= byte)
 
+
+typedef struct mqtt_packet {
+	const uint8_t * packet;  //!< pointer to all packet data
+	uint32_t length;  //!< length of the whole packet
+} mqtt_packet_t;
+
+/*  \brief pack connect packet 
+ *  \param p_conn_flag pointer to conn_flag structure
+ *  \param keep_alive time limit to keep alive measured by seconds 
+ *  \param id_client client identifier string
+ *  \param w_topic will topic string
+ *  \param w_msg will message string
+ *  \param user_name user name string
+ *  \param pwd password string
+ *  \retval mqtt packet
+ * */
+struct mqtt_packet * mqtt_packet_connect(
+		//!< fixed header
+		
+		//!< variable header
+		const struct mqtt_connect_flag * p_conn_flag, 
+		uint16_t keep_alive,
+
+		//!< payload
+		const char * id_client,
+		const char * w_topic,
+		const char * w_msg,
+		const char * user_name,
+		const char * pwd
+		);
+
+/*  \brief pack mqtt connack packet
+ *  \param p_connack_flag flags of connack
+ *  \param ret_code return code of connect
+ *  \retval mqtt packet
+ * */
+struct mqtt_packet * mqtt_packet_connack(
+		//!< fixed header
+		
+		//!< variable header
+		const struct mqtt_connack_flag * p_connack_flag,
+		enum mqtt_conn_ret_code ret_code
+		//!< payload
+		);
+
+/*  \brief pack mqtt publish packet
+ *  \param p_ctl_flag flag of mqtt packet control
+ *  \param topic_name name of topic publish to 
+ *  \param id_packet identifier of packet
+ *  \param app_msg application specify message
+ *  \retval mqtt packet
+ * */
+struct mqtt_packet * mqtt_packet_publish(
+		//!< fixed header
+		const struct mqtt_ctl_head * p_ctl_flag,
+		//!< variable header
+		const char * topic_name,
+		uint16_t id_packet,
+		//!< payload
+		const char * app_msg
+		);
+
+/*  \brief pack mqtt publish acknowledge packet
+ *  \param id_packet identifier of packet
+ *  \retval mqtt packet
+ * */
+struct mqtt_packet * mqtt_packet_puback(
+		//!< fixed header
+		
+		//!< variable header
+		uint16_t id_packet
+		//!< payload
+		);
+
+/*  \brief pack mqtt publish received packet
+ *  \param id_packet identifier of packet
+ *  \retval mqtt packet
+ * */
+struct mqtt_packet * mqtt_packet_pubrec(
+		//!< fixed header
+		//!< variable header
+		uint16_t id_packet
+		//!< payload
+		);
+
+/*  \brief pack mqtt publish release packet
+ *  \param id_packet identifier of packet
+ *  \retval mqtt packet
+ * */
+struct mqtt_packet * mqtt_packet_pubrel(
+		//!< fixed header
+		//!< variable header
+		uint16_t id_packet
+		//!< payload
+		);
+
+/*  \brief pack mqtt publish complete packet
+ *  \param id_packet identifier of packet
+ *  \retval mqtt packet
+ * */
+struct mqtt_packet * mqtt_packet_pubcomp(
+		//!< fixed header
+		//!< variable header
+		uint16_t id_packet
+		//!< payload
+		);
+
+/*  \brief pack mqtt subscribe packet
+ *  \param id_packet identifier of packet
+ *  \param sub_content content array to subscribe 
+ *  \param sub_count count of subscribe
+ *  \retval mqtt packet
+ * */
+struct mqtt_packet * mqtt_packet_subscribe(
+		//!< fixed header
+		//!< variable header
+		uint16_t id_packet,
+		//!< payload
+		struct mqtt_packet_subscribe_content sub_content[],
+		uint16_t sub_count
+		);
+
+/*  \brief pack mqtt suback packet
+ *  \param id_packet identifier of packet
+ *  \param suback_flag[] flags of suback array
+ *  \param suback_flag_count count of suback
+ *  \retval mqtt packet
+ * */
+struct mqtt_packet * mqtt_packet_suback(
+		//!< fixed header
+		//!< variable header
+		uint16_t id_packet,
+		//!< payload
+		struct mqtt_suback_flag suback_flag[],
+		uint16_t suback_flag_count
+		);
+
+/*  \brief pack mqtt unsubscribe packet
+ *  \param id_packet identifier of packet
+ *  \param top_filter[] array of top_filter string
+ *  \param to_filter_count count of top_filter string
+ *  \retval mqtt packet
+ * */
+struct mqtt_packet * mqtt_packet_unsubscribe(
+		//!< fixed header
+		//!< variable header
+		uint16_t id_packet
+		//!< payload
+		const char * top_filter[],
+		uint16_t top_filter_count
+		);
+
+/*  \brief pack mqtt unsuback packet
+ *  \param id_packet identifier of packet
+ * */
+struct mqtt_packet * mqtt_packet_unsuback(
+		//!< fixed header
+		//!< variable header
+		uint16_t id_packet,
+		//!< payload
+		);
+
+/*  \brief pack mqtt pingreq packet
+ *  \retval mqtt packet
+ * */
+struct mqtt_packet * mqtt_packet_pingreq(
+		//!< fixed header
+		//!< variable header
+		//!< payload
+		);
+
+/*  \brief pack mqtt pingresp packet
+ *  \retval mqtt packet
+ * */
+struct mqtt_packet * mqtt_packet_pingresp(
+		//!< fixed header
+		//!< variable header
+		//!< payload
+		);
+
+/*  \brief pack mqtt disconnect packet
+ *  \retval mqtt packet
+ * */
+struct mqtt_packet * mqtt_packet_disconnect(
+		//!< fixed header
+		//!< variable header
+		//!< payload
+		);
+
+
 #ifdef __cplusplus
 	}
 #endif
