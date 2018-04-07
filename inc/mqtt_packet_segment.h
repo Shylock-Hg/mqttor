f14e3dd538132363de294dcbbb5998153715e8c2
shylock <tcath2s@gmail.com>
2018-04-07 21:40:30 +0800

    init with segment operitons

---
+++ inc/mqtt_packet_segment.h
@@ -0,0 +1,32 @@
+#ifndef _MQTT_PACKET_SEGMENT_H_
+#define _MQTT_PACKET_SEGMENT_H_
+
+#ifdef __cplusplus
+	extern "C" {
+#endif
+
+#include <assert.h>
+
+/*! \brief evaluate the value of bits in uint variable
+ *  \param uint the packed uint value
+ *  \param msk the and operator mask
+ *  \param offset the bits offset to bit 0
+ * */
+#define MQTT_PACKET_SEGMENT_EVAL(uint,msk,offset) ((uint&msk) >> offset)
+
+/*! \brief check packet flag value of boolean type
+ *  \param byte value of boolean
+ * */
+#define MQTT_PACKET_FLAG_VAL_BOOL_CHECK(byte) assert(0 == byte || 1 == byte)
+
+/*! \brief check packet flag QoS value 
+ *  \param byte value of QoS
+ * */
+#define MQTT_PACKET_FLAG_VAL_QoS_CHECK(byte)  assert(0 <= byte && 2 >= byte)
+
+#ifdef __cplusplus
+	}
+#endif
+
+#endif //!< _MQTT_PACKET_SEGMENT_H_
+
